313124d32d1d6b11844822206dbd42e9ed75fd49
Ammar Ahmed Mostafa <ammarx3@icloud.com>
2025-12-14 12:06:45 +0200

    ED

---
+++ assets/app/header.js
@@ -0,0 +1,53 @@
+
+let header = document.getElementById("header");
+
+
+
+header.innerHTML = `  
+  <header>
+        <div class="header-wrapper">
+  <div class="container-default w-container">
+    <div class="header-content-wrapper">
+      <div class="header-left-side">
+        <a href="https://3mmarx3.github.io/Cryptography/" class="header-logo-link w-nav-brand">
+          <img src="https://cdn.prod.website-files.com/64d2cc2d27b51cf1b517c011/6616b1c13ffb5841a23a15ea_logo-cryptoverse-x-webflow-template.png" alt="Logo - Cryptoverse X Webflow Template">
+        </a>
+
+        <nav class="header-nav-menu-wrapper">
+          <ul class="header-nav-menu-list">
+            <li class="header-nav-list-item">
+              <a href="https://3mmarx3.github.io/Cryptography/" class="header-nav-link">Home</a>
+            </li>
+            <li class="header-nav-list-item">
+              <a href="https://eng-ammar.com/page/about.php" class="header-nav-link">About</a>
+            </li>
+            <li class="header-nav-list-item">
+              <a href="https://eng-ammar.com/page/project.php" class="header-nav-link">Blog</a>
+            </li>
+            <li class="header-nav-list-item">
+              <a href="https://eng-ammar.com" class="btn-primary small w-button">Get started</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+<div class="header-right-side">
+  <div class="social-media-links-container">
+    <a href="https://www.facebook.com/3mmarx3/" target="_blank" class="social-link-single" aria-label="Facebook">
+      <i class="bi bi-facebook" aria-hidden="true"></i>
+    </a>
+
+    <a href="https://www.instagram.com/3mmarx3/" target="_blank" class="social-link-single" aria-label="Instagram">
+      <i class="bi bi-instagram" aria-hidden="true"></i>
+    </a>
+    <a href="https://www.linkedin.com/in/eng-ammar-ahmed/" target="_blank" class="social-link-single" aria-label="LinkedIn">
+      <i class="bi bi-linkedin" aria-hidden="true"></i>
+    </a>
+  </div>
+</div>
+
+    </div>
+  </div>
+</div>
+  </header>
+`;
